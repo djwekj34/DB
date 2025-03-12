3a54d2e27213b4fc2525621a1d23de382fbeba0b
djwekj34 <193445559+djwekj34@users.noreply.github.com>
2025-03-12 12:47:17 +0900

    Add files via upload

---
+++ 20416허준기.sql
@@ -0,0 +1,8 @@
+CREAT TABLE Address(
+ Address1 varchar(255),
+ Address2 varchar(255),
+ Address3 varchar(255),
+ Postal_Code varchar(255),
+ Client_ID varchar(255);
+insert into address values('안야시','동안구','학의로','12345','이름');
+ drop table address;//테이블삭제
