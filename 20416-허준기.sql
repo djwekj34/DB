134db57d9f1f81a6e6e22069393644b2e325b76f
djwekj34 <193445559+djwekj34@users.noreply.github.com>
2025-11-07 16:29:56 +0900

    Add files via upload

--- 20416-허준기.sql
+++ 20416-허준기.sql
@@ -1,33 +1,35 @@
-create table new_table
-(no number(3),name varchar2(10),birth DATE);
-desc new_table;
-select *from new_table;
-
-create table tt02
-(no number(3,1) default 0,name varchar2(10) default 'No Name',
-hiredate date default sysdate);
-
-INSERT INTO tt02(no) values(1);
-select*from tt02;
-
-CREATE TABLE 한글테이블
-(컬럼1 number,컬럼2 varchar2(10),컬럼3 date);
-select*from 한글테이블;
-
-select*from dept2;
-
 CREATE TABLE dept3
 AS
-select*from dept2;
-select*from dept23;
+select * from dept2;
+select * from dept3;
+
 CREATE TABLE dept4
 AS
 select dcode,dname from dept2;
-select*from dept4;
-create table dept5
-as select*from dept2 where 1=2;
-select*from dept2;
-create table dept6
-as
+select * from dept4;
+
+drop table dept5;
+
+CREATE TABLE dept5
+AS
+select * from dept2 where dcode=1002;
+select * from dept2;
+
+CREATE TABLE dept6
+AS
 select dcode,dname from dept2 where dcode IN(1000,1001,1002);
 select * from dept6;
+
+CREATE TABLE dept7
+as
+select * from dept2 where 1=2;
+
+select * from dept2 where 1=2;
+select*from dept6;
+
+alter table dept6
+ add(location varchar2(10));
+ 
+ alter table dept6
+ add(location varchar2(10)default'SEOUL');
+
